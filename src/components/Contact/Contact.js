864e23c3d7ec9ebf220933f771b81ba763c69cc9
georgosjohn <GeorgosJohn@JohnDeere.com>
2020-11-13 18:04:46 -0600

    contact styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -51,91 +51,92 @@ class Contact extends Component{
         console.log(this.state)
     return(
         <div className="Contact-Container"> 
-            <section>
-                <h2>Contact Delta Painting Inc.</h2>
-                <p className="inputDescription">Please fill out the form below to contact Delta Painting, Inc. When filling out the form please input your most recent and valid contact information so Delta Painting, Inc. can contact you back as soon as possible.  </p>
-                <p>Please fill out the form below.</p>
-                <form className="ContactInfo" onSubmit={this.handleSubmit}>
-                    <div>
-                        First Name: 
-                        <input className = 'firstName'
-                            type = 'text'
-                            name = 'firstName'
-                            placeholder = 'Enter First Name'
-                            value={this.state.firstName}
-                            onChange = {this.handleChange}
-                            onSubmit={this.handleSubmit}
-                        />                        
-                    </div>
+            <div className="Contact-UserInput-Container">
+                <section>
+                    <h2>Contact Delta Painting Inc.</h2>
+                    <p className="inputDescription">Please fill out the form below to contact Delta Painting, Inc. When filling out the form please input your most recent and valid contact information so Delta Painting, Inc. can contact you back as soon as possible.  </p>
+                    <p>Please fill out the form below.</p>
+                    <form className="ContactInfo" onSubmit={this.handleSubmit}>
+                        <div>
+                            First Name: 
+                            <input className = 'firstName'
+                                type = 'text'
+                                name = 'firstName'
+                                placeholder = 'Enter First Name'
+                                value={this.state.firstName}
+                                onChange = {this.handleChange}
+                                onSubmit={this.handleSubmit}
+                            />                        
+                        </div>
 
-                    <div>
-                    Last Name: 
-                        <input className = 'lastName'
-                            type = 'text'
-                            name = 'lastName'
-                            placeholder = 'Enter Last Name'
-                            value={this.state.lastName}
-                            onChange = {this.handleChange}
-                            onSubmit={this.handleSubmit}
-                        />                        
-                    </div>
+                        <div>
+                        Last Name: 
+                            <input className = 'lastName'
+                                type = 'text'
+                                name = 'lastName'
+                                placeholder = 'Enter Last Name'
+                                value={this.state.lastName}
+                                onChange = {this.handleChange}
+                                onSubmit={this.handleSubmit}
+                            />                        
+                        </div>
 
-                    <div>
-                    Email Address: 
-                        <input className = 'emailAddress'
-                            type = 'text'
-                            name = 'emailAddress'
-                            placeholder = 'Enter Valid Email Address'
-                            value={this.state.emailAddress}
-                            onChange = {this.handleChange}
-                            onSubmit={this.handleSubmit}
-                        />                        
-                    </div>
+                        <div>
+                        Email Address: 
+                            <input className = 'emailAddress'
+                                type = 'text'
+                                name = 'emailAddress'
+                                placeholder = 'Enter Valid Email Address'
+                                value={this.state.emailAddress}
+                                onChange = {this.handleChange}
+                                onSubmit={this.handleSubmit}
+                            />                        
+                        </div>
 
-                    <div>
-                    Phone: 
-                        <input className = 'phone'
-                            type = 'text'
-                            name = 'phone'
-                            placeholder = 'Enter Phone Number'
-                            value={this.state.phone}
-                            onChange = {this.handleChange}
-                            onSubmit={this.handleSubmit}
-                        />                        
-                    </div>
+                        <div>
+                        Phone: 
+                            <input className = 'phone'
+                                type = 'text'
+                                name = 'phone'
+                                placeholder = 'Enter Phone Number'
+                                value={this.state.phone}
+                                onChange = {this.handleChange}
+                                onSubmit={this.handleSubmit}
+                            />                        
+                        </div>
 
-                    <div>
-                    Job Estimate: 
-                        <select className = 'jobEstimate'
-                            value={this.state.onChange}
-                            name="jobEstimate"
-                            value={this.state.jobEstimate}
-                            onChange = {this.handleChange}
-                            onSubmit={this.handleSubmit}
-                            >
-                            <option value="Select...">Select...</option>
-                            <option value="Yes">Yes</option>
-                            <option value="No">No</option>
-                        </select>
-                    </div>
+                        <div>
+                        Job Estimate: 
+                            <select className = 'jobEstimate'
+                                value={this.state.onChange}
+                                name="jobEstimate"
+                                value={this.state.jobEstimate}
+                                onChange = {this.handleChange}
+                                onSubmit={this.handleSubmit}
+                                >
+                                <option value="Select...">Select...</option>
+                                <option value="Yes">Yes</option>
+                                <option value="No">No</option>
+                            </select>
+                        </div>
 
-                    <div>
-                    Reason of Contact: 
-                    {/* <br/> */}
-                        <input className = 'reasonOfContact'
-                            type = 'textarea'
-                            name = 'reasonOfContact'
-                            placeholder = 'Enter Your Reasoning for Contacting Delta Painting, Inc.'
-                            value={this.state.reasonOfContact}
-                            onChange = {this.handleChange}
-                            onSubmit={this.handleSubmit}
-                        />                        
-                    </div>
-                    
-                    <input className="submitButton" type="submit"/>
-
-                </form>
-            </section>
+                        <div>
+                        Reason of Contact: 
+                        {/* <br/> */}
+                            <input className = 'reasonOfContact'
+                                type = 'textarea'
+                                name = 'reasonOfContact'
+                                placeholder = 'Enter Your Reasoning for Contacting Delta Painting, Inc.'
+                                value={this.state.reasonOfContact}
+                                onChange = {this.handleChange}
+                                onSubmit={this.handleSubmit}
+                            />                        
+                        </div>
+                        
+                        <input className="submitButton" type="submit"/>
+                    </form>
+                </section>
+            </div>
         </div>
     )        
     }
